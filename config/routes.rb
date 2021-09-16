722fbcf99f4e6253e7a480a0a16ba3187d45bdf6
chinamiii <chinami.2929@icloud.com>
2021-09-16 04:13:48 +0000

    [Add routes]

--- config/routes.rb
+++ config/routes.rb
@@ -1,55 +1,71 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'members/show'
-  end
-  get 'homes/top'
+
+
+   devise_for :members, controllers: {
+    sessions:      'public/members/sessions',
+    passwords:     'public/members/passwords',
+    registrations: 'public/members/registrations'
+   }
+
+
+
+   devise_for :admins, controllers: {
+    sessions:      'admin/admins/sessions',
+    passwords:     'admin/admins/passwords',
+    registrations: 'admin/admins/registrations'
+   }
+
+
+  root 'homes#top'
+
+
   get 'homes/about'
-  devise_for :members, controllers: {
-    sessions:      'members/sessions',
-    passwords:     'members/passwords',
-    registrations: 'members/registrations'
-  }
-
-  devise_for :admins, controllers: {
-    sessions:      'admins/sessions',
-    passwords:     'admins/passwords',
-    registrations: 'admins/registrations'
-  }
 
   namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
+    resources :genres, only: [:index, :create, :edit, :update]
   end
+
   namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
+
   namespace :admin do
-    get 'members/index'
-    get 'members/show'
-    get 'members/edit'
+    resources :members, only: [:index, :show, :edit, :update]
   end
+
   namespace :admin do
-    get 'orders/index'
-    get 'orders/show'
+    resources :orders, only: [:index, :show, :update]
+  end
+
+  namespace :admin do
+    resources :order_details, only: [:update]
+  end
+
+  namespace :public do
+    resources :deliveries, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  namespace :public do
+    resources :orders, only: [:index, :new, :show, :create]
+    get 'orders/confirm' => 'orders#confirm', as: 'confirm'
+    get 'orders/complete' => 'orders#complete', as: 'complete'
+  end
+
+  namespace :public do
+    resources :cart_items, only: [:index, :create, :destroy, :update]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all'
+  end
+
+  namespace :public do
+    resources :items, only: [:index, :show]
   end
   namespace :public do
-    get 'deliveries/index'
-    get 'deliveries/edit'
+    resources :members, only: [:edit, :update]
+    get 'members/mypage' => 'members#show', as: 'mypage'
+    get 'members/unsubscribe' => 'members#unsubscribe', as: 'unsubscribe'
+    patch 'members/withdrawal' => 'members#withdrawal', as: 'withdrawal'
+
   end
-  namespace :public do
-    get 'order/index'
-    get 'order/show'
-    get 'order/new'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
