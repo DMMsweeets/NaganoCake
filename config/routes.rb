531ef80f8052b7de623d3a315f6d034ec668b3f7
chinamiii <chinami.2929@icloud.com>
2021-09-21 02:27:46 +0000

    [test]

--- config/routes.rb
+++ config/routes.rb
@@ -71,5 +71,9 @@ Rails.application.routes.draw do
     resources :members, only: [:edit, :update]
   end
 
+  namespace :public do
+    resources :genres, only: [:show]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
